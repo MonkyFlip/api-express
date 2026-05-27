04f469ce305e1490a9c34a1f7be2688ee7e2bb9b
MonkyFlip <miguelhercerv@gmail.com>
2026-05-26 19:22:42 -0600

    ECMAScript 6

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('hola mundo');
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`escucha desde el puerto ${PORT}`)
+});
+
+console.log(express);
