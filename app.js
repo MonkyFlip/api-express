7e49c05921f8df38fbebcc9a4c2a205c86a748ba
MonkyFlip <miguelhercerv@gmail.com>
2026-05-26 19:23:33 -0600

    si

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 
 dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('hola mundo');
+app.get("/", (req, res) => {
+  res.send("hola mundo");
 });
 
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-    console.log(`escucha desde el puerto ${PORT}`)
+  console.log(`escucha desde el puerto ${PORT}`);
 });
 
 console.log(express);
